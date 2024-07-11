3a55ed5198063d1a7c257a2d6af363f6fa3ec634
Pedro Arruda <parruda@findhotel.net>
2024-07-11 13:58:51 -0300

    important build fixed for linux

--- utils/build-linux-wheels.sh
+++ utils/build-linux-wheels.sh
@@ -1,13 +1,52 @@
 #! /usr/bin/env bash
 
+set -e
+
 docker run -i                                   \
     --platform linux/amd64 --rm -v $(pwd):/io   \
     --entrypoint /bin/bash ghcr.io/pyo3/maturin \
 <<EOF
-    cd jyafn-python
-    maturin build -i=3.8
-    maturin build -i=3.9
-    maturin build -i=3.10
-    maturin build -i=3.11
-    maturin build -i=3.12
+    cd vendored/qbe
+    make clean
+    make qbe
+    ./qbe --help
+    cd ../../jyafn-python
+    maturin build --release -i=3.8
+    maturin build --release -i=3.9
+    maturin build --release -i=3.10
+    maturin build --release -i=3.11
+    maturin build --release -i=3.12
+EOF
+
+docker run -i                                   \
+    --platform linux/amd64 --rm -v $(pwd):/work \
+    --entrypoint /bin/bash python:3.10          \
+<<EOF
+    set -e
+    cd /work/target/wheels/
+    ls . | grep 310 | grep manylinux | xargs pip install
+    cd ../../jyafn-python
+    python tests/serde.py
+EOF
+
+docker run -i                                   \
+    --platform linux/amd64 --rm -v $(pwd):/work \
+    --entrypoint /bin/bash python:3.11          \
+<<EOF
+    set -e
+    cd /work/target/wheels/
+    ls . | grep 311 | grep manylinux | xargs pip install
+    cd ../../jyafn-python
+    python tests/serde.py
+EOF
+
+docker run -i                                   \
+    --platform linux/amd64 --rm -v $(pwd):/work \
+    --entrypoint /bin/bash python:3.12          \
+<<EOF
+    set -e
+    cd /work/target/wheels/
+    ls . | grep 312 | grep manylinux | xargs pip install
+    cd ../../jyafn-python
+    python tests/serde.py
 EOF
--- utils/build-macos-wheels.sh
+++ utils/build-macos-wheels.sh
@@ -1,3 +1,7 @@
+cd vendored/qbe
+make clean
+make qbe
+cd ../../jyafn-python
 maturin build --release -i=3.8
 maturin build --release -i=3.9
 maturin build --release -i=3.10
